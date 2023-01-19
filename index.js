b3a95b8132d8eb0e31f997d694e7b72adc598e8c
Kelvin-owit <owitkelvin@gmail.com>
2023-01-19 11:46:37 +0300

    second commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// java script for toggle menu
+var navlinks = document.getElementById("navlinks");
+function showMenu() {
+  navlinks.style.display = "block";
+}
+function hideMenu() {
+  navlinks.style.display = "none";
+}
